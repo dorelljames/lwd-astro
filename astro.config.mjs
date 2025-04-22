ad1c82bcb64b963d5f69aaf6cddf193b4cfdf69d
Dorell James <galangdj@gmail.com>
2025-04-22 20:39:45 +0800

    Enhance Astro project with Google Fonts integration, add typography plugin, and update homepage layout. Introduced logo asset, refined global styles, and improved configuration for better font handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Noto Sans",
+        cssVariable: "--font-noto-sans",
+        weights: ["400", "500", "600", "700"],
+      },
+      // {
+      //   provider: fontProviders.fontshare(),
+      //   name: "Chubbo",
+      //   cssVariable: "--font-chubbo",
+      //   weights: ["400", "500", "600", "700"],
+      // },
+      // {
+      //   provider: fontProviders.fontshare(),
+      //   name: "Supreme",
+      //   cssVariable: "--font-supreme",
+      //   weights: ["400", "500", "600", "700"],
+      // },
+    ],
+  },
 });
